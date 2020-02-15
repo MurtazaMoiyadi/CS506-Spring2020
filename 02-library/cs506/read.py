7dc4dd6d0fcfa7c471f02e1f74023f1706ad481d
Murtaza Moiyadi <murtaza.moiyadi98@gmail.com>
2020-02-15 17:18:20 -0500

    first commit

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,11 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    results = []
+    with open(csv_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            results.append([eval(x) for x in line.split(',')])
+
+    return results
+            
